c666cfcc1b8298ae2bad33375f27df44ae0a71ed
‘zhandehuang’ <it_zdh@163.com>
2023-07-04 14:18:28 +0800

    .

---
+++ vue-test/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <School :getSchoolName="getSchoolName"/>
+</template>
+
+<script>
+import School from './components/School'
+
+export default {
+  name: 'App',
+  components: {
+    School
+  },
+  methods:{
+    getSchoolName(name){
+      console.log("App组件收到了学校名，学校名称是："+name)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-test/src/components/School.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>学校:{{name}}</h2>
+    <h2>地址:{{address}}</h2>
+    <button @click="getSchoolNametoApp">点我把学校名传递给父组件</button>
+  </div>
+</template>
+
+<script>
+  export default{
+    data(){
+      return {
+        name:'清华大学',
+        address:'北京'
+      }
+    },
+    props:['getSchoolName'],
+    methods:{
+      getSchoolNametoApp(){
+        this.getSchoolName(this.name)
+      }
+    }
+  }
+</script>
