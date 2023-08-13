03de1fba5f413fd93f7eb199fee0cf28e23c7179
‘ZhengHongyi'’ <1111@163.com>
2023-08-13 23:29:06 +0800

    作用域插槽

---
+++ test-scope-slot/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+
+    <MyCategory title="电影" >
+      <template scope="jdjm">
+<!--        {{jdjm}}-->
+        <ul>
+          <li v-for="(item,index) in jdjm.films" :key="index">{{item}}</li>
+        </ul>
+        <h3>{{jdjm.message}}</h3>
+      </template>
+    </MyCategory>
+    <MyCategory title="游戏" >
+      <template slot-scope="jdjm">
+        <!--        {{jdjm}}-->
+        <ol>
+          <li v-for="(item,index) in jdjm.games" :key="index">{{item}}</li>
+        </ol>
+        <h3>{{jdjm.message}}</h3>
+      </template>
+    </MyCategory>
+  </div>
+</template>
+
+<script>
+
+import MyCategory from "@/components/MyCategory";
+
+
+export default {
+  name: 'App',
+  data(){
+
+  },
+  components: {
+    MyCategory
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ test-scope-slot/src/components/MyCategory.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="category">
+    <h3>{{title}}分类</h3>
+    <slot :films="films" :games="games" message="这是一条消息"></slot>
+  </div>
+</template>
+<script>
+export default{
+  props:['title'],
+  data(){
+    return {
+      films:["《消失的她》","《孤注一掷》","《灌篮高手》"],
+      games:['GTAV','只狼']
+    }
+  }
+}
+</script>
+
+<style>
+.category{
+  background-color: skyblue;
+  width: 200px;
+  height: 300px;
+}
+h3{
+  text-align: center;
+  background-color: orange;
+}
+
+</style>
